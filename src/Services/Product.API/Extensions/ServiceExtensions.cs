400f03ada57be560311351ac1c1a4687479fc248
QuangDuy <justinquangduy@gmail.com>
2022-10-03 16:34:55 +0700

    Create Extensions file

---
+++ src/Services/Product.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Product.API.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+}
--- src/Services/Product.API/Program.cs
+++ src/Services/Product.API/Program.cs
@@ -1,34 +1,23 @@
 using Common.Logging;
+using Product.API.Extensions;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Host.UseSerilog(Serilogger.Configure);
 
 Log.Information("Start Product API up");
 
 try
 {
+    builder.Host.UseSerilog(Serilogger.Configure);
+    builder.Host.AddAppConfigurations();
+    
     // Add services to the container.
-
-    builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+    builder.Services.AddInfrastructure();    
 
     var app = builder.Build();
 
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+    app.UseInfrastructure();
 
-    app.UseHttpsRedirection();
-
-    app.UseAuthorization();
-
-    app.MapControllers();
 
     app.Run();
 }
